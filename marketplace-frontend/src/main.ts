2b41ed18b59e24aef18dc964841d35da1ad1dcf6
ct222ct <72874201+ct222ct@users.noreply.github.com>
2025-01-10 23:34:05 +0100

    Update Frontend

--- marketplace-frontend/src/main.ts
+++ marketplace-frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()], // Stellt HttpClient global bereit
+  providers: [provideHttpClient(withFetch())], // Aktiviert die Nutzung von fetch
 }).catch(err => console.error(err));
